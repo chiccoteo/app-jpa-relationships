a3043a8c242bdb624cfaad058dad2d8c0648c0b3
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-03-13 17:43:53 +0500

    Student CRUD and Get Students for faculty and group

--- src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
+++ src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
@@ -5,14 +5,35 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import uz.pdp.appjparelationships.entity.Address;
+import uz.pdp.appjparelationships.entity.Group;
 import uz.pdp.appjparelationships.entity.Student;
+import uz.pdp.appjparelationships.entity.Subject;
+import uz.pdp.appjparelationships.payload.StudentDto;
+import uz.pdp.appjparelationships.repository.AddressRepository;
+import uz.pdp.appjparelationships.repository.GroupRepository;
 import uz.pdp.appjparelationships.repository.StudentRepository;
+import uz.pdp.appjparelationships.repository.SubjectRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
     @Autowired
     StudentRepository studentRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Autowired
+    GroupRepository groupRepository;
+
+    @Autowired
+    SubjectRepository subjectRepository;
+
 
     //1. VAZIRLIK
     @GetMapping("/forMinistry")
@@ -42,7 +63,110 @@ public class StudentController {
     }
 
     //3. FACULTY DEKANAT
-    //4. GROUP OWNER
+    @GetMapping("/forFaculty/{facultyId}")
+    public Page<Student> getStudentsForFaculty(@PathVariable Integer facultyId,
+                                               @RequestParam int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return studentRepository.findAllByGroup_FacultyId(facultyId, pageable);
+    }
 
+    //4. GROUP OWNER
+    @GetMapping("/forGroup/{groupId}")
+    public Page<Student> getStudentsForGroup(@PathVariable Integer groupId,
+                                             @RequestParam int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return studentRepository.findAllByGroupId(groupId, pageable);
+    }
+
+
+    @PostMapping("/save")
+    public String save(@RequestBody StudentDto studentDto) {
+        Optional<Address> optionalAddress = addressRepository.findById(studentDto.getAddressId());
+        if (optionalAddress.isPresent()) {
+            Optional<Group> optionalGroup = groupRepository.findById(studentDto.getGroupId());
+            if (optionalGroup.isPresent()) {
+                boolean haveAllSubject = false;
+                for (Integer subjectId : studentDto.getSubjectIds()) {
+                    Optional<Subject> optionalSubject = subjectRepository.findById(subjectId);
+                    if (optionalSubject.isPresent()) {
+                        haveAllSubject = true;
+                    } else {
+                        haveAllSubject = false;
+                        break;
+                    }
+                }
+                if (haveAllSubject) {
+                    List<Subject> subjects = new ArrayList<>();
+                    for (Integer subjectId : studentDto.getSubjectIds()) {
+                        Optional<Subject> optionalSubject = subjectRepository.findById(subjectId);
+                        optionalSubject.ifPresent(subjects::add);
+                    }
+                    Student student = new Student();
+                    student.setSubjects(subjects);
+                    student.setAddress(optionalAddress.get());
+                    student.setGroup(optionalGroup.get());
+                    student.setFirstName(studentDto.getFirstName());
+                    student.setLastName(studentDto.getLastName());
+                    studentRepository.save(student);
+                    return "Saved";
+                }
+            } else {
+                return "Such group not found";
+            }
+        }
+        return "Such address not found";
+    }
+
+    @PutMapping("/edit/{id}")
+    public String editById(@PathVariable Integer id, @RequestBody StudentDto studentDto) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            Optional<Address> optionalAddress = addressRepository.findById(studentDto.getAddressId());
+            if (optionalAddress.isPresent()) {
+                Optional<Group> optionalGroup = groupRepository.findById(studentDto.getGroupId());
+                if (optionalGroup.isPresent()) {
+                    boolean haveAllSubject = false;
+                    for (Integer subjectId : studentDto.getSubjectIds()) {
+                        Optional<Subject> optionalSubject = subjectRepository.findById(subjectId);
+                        if (optionalSubject.isPresent()) {
+                            haveAllSubject = true;
+                        } else {
+                            haveAllSubject = false;
+                            break;
+                        }
+                    }
+                    if (haveAllSubject) {
+                        List<Subject> subjects = new ArrayList<>();
+                        for (Integer subjectId : studentDto.getSubjectIds()) {
+                            Optional<Subject> optionalSubject = subjectRepository.findById(subjectId);
+                            optionalSubject.ifPresent(subjects::add);
+                        }
+                        Student student = optionalStudent.get();
+                        student.setSubjects(subjects);
+                        student.setAddress(optionalAddress.get());
+                        student.setGroup(optionalGroup.get());
+                        student.setFirstName(studentDto.getFirstName());
+                        student.setLastName(studentDto.getLastName());
+                        studentRepository.save(student);
+                        return "Edited";
+                    }
+                } else {
+                    return "Such group not found";
+                }
+            }
+            return "Such address not found";
+        }
+        return "Such student not found";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteById(@PathVariable Integer id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            studentRepository.deleteById(id);
+            return "Deleted";
+        }
+        return "Such student not found";
+    }
 
 }
